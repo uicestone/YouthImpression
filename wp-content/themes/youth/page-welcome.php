79b157c97b779a2c27b62d9c23db1a2b52e33796
supersheep <supersheepxjy@gmail.com>
2013-12-21 23:46:05 +0800

    welcome animate

--- wp-content/themes/youth/page-welcome.php
+++ wp-content/themes/youth/page-welcome.php
@@ -6,25 +6,53 @@
 <?php wp_head(); ?>
 </head>
 <body>
+<style>
+#welcome-bar{
+    position: relative;
+}
+#welcome-bar .welcome_pic{
+    position: absolute;
+    top: 0;
+    height:230px;
+    overflow: hidden;
+}
+
+#welcome-bar .welcome_pic ul{
+    width:2000px;
+    left:0;
+    position: absolute;
+}
+#welcome-bigPic{
+    position: relative;
+}
+#enter-btn{
+    width:240px;
+    height:160px;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+}
+</style>
 <div id="home-wrap">
 <div id="welcome-bg">
     <div id="welcome-bar">
         <div id="welcome-bigPic" class="bigPic-welcome">
-            <a href="#">
-                <img src="<?= get_template_directory_uri(); ?>/images/welcome-bigPic.png" width="831" height="552" /></a>
+            <img src="<?= get_template_directory_uri(); ?>/images/welcome-bigPic.png" width="831" height="552" />
+
+            <a href="#" id="enter-btn"></a>
         </div>
         <div class="welcome_pic">
             <ul>
                 <li>
                     <img src="<?= get_template_directory_uri(); ?>/images/welcome-p1.png" width="148" height="224" />
                 </li>
-                <li style="margin-right:775px;">
+                <li>
                     <img src="<?= get_template_directory_uri(); ?>/images/welcome-p2.png" width="148" height="224" />
                 </li>
                 <li>
                     <img src="<?= get_template_directory_uri(); ?>/images/welcome-p3.png" width="148" height="224" />
                 </li>
-                <li style="margin-right:0;">
+                <li>
                     <img src="<?= get_template_directory_uri(); ?>/images/welcome-p4.png" width="148" height="224" />
                 </li>
             </ul>
@@ -35,5 +63,35 @@
             <img src="<?= get_template_directory_uri(); ?>/images/index-weibo.png" width="171" height="39" /></a>
     </div>
 </div>
+
+<script>
+(function(){
+    var container = $(".welcome_pic");
+    var ul = container.find("ul");
+    var items = $(".welcome_pic li");
+    var count = items.length;
+    var one_width = (items.width() + parseInt(items.css("margin-right")));
+    var width =  one_width * count;
+    var maxwidth = container.width();
+    while(width < maxwidth){
+        for(var i = 0; i < count; i++){
+            ul.append(items.eq(i).clone());
+            width += one_width;
+        }
+    }
+
+    function scroll(){
+        ul.animate({
+            left: - one_width * 4
+        },10000,'linear',function(){
+            ul.css("left","0");
+            scroll();
+        });
+    }
+    
+    scroll();
+})();
+
+</script>
 
 <?php get_footer(); ?>
