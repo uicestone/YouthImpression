99a49283bca2e47f7981dc8554d1b3e007e2e500
supersheep <supersheepxjy@gmail.com>
2013-12-22 20:13:53 +0800

    add back mediaelement styles

--- wp-content/themes/youth/functions.php
+++ wp-content/themes/youth/functions.php
@@ -28,6 +28,7 @@ add_action( 'wp_enqueue_scripts', function(){
 	
 	wp_register_style('style', get_template_directory_uri().'/style.css');
 	wp_enqueue_style('style');
+	wp_enqueue_style('mediaelement');
 });
 
 /**
