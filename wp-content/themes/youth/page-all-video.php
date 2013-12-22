16c9460d1ea34c5c3c45b29227a23ea193246402
supersheep <supersheepxjy@gmail.com>
2013-12-22 14:24:59 +0800

    use mediaelement fix #6

--- wp-content/themes/youth/page-all-video.php
+++ wp-content/themes/youth/page-all-video.php
@@ -1,5 +1,10 @@
 <?php get_header(); ?>
 <link rel="stylesheet" href="<?= get_template_directory_uri(); ?>/css/waterfall.css">
+<link rel='stylesheet' id='mediaelement-css'  href='http://qcyh.lc/wp-includes/js/mediaelement/mediaelementplayer.min.css?ver=2.13.0' type='text/css' media='all' />
+<link rel='stylesheet' id='wp-mediaelement-css'  href='http://qcyh.lc/wp-includes/js/mediaelement/wp-mediaelement.css?ver=3.8' type='text/css' media='all' />
+<script type='text/javascript' src='http://qcyh.lc/wp-includes/js/mediaelement/mediaelement-and-player.min.js?ver=2.13.0'></script>
+<script type='text/javascript' src='http://qcyh.lc/wp-includes/js/mediaelement/wp-mediaelement.js?ver=3.8'></script>
+<!--[if lt IE 9]><script>document.createElement('video');</script><![endif]-->
 <style>
 #video-banner .prev,#video-banner .next{
 -webkit-user-select: none;  /* Chrome all / Safari all */
@@ -9,20 +14,36 @@
 -o-user-select: none;
 user-select: none;
 }
+#pop .video-container{
+	width: 848px; 
+	height: 500px;
+	float:right;
+	position: relative;
+}
+
+#pop .video-container img{
+	width: 848px; 
+	height: 500px;
+	position: absolute;
+	top: 0;
+	left: 0;
+}
+#pop .mejs-container{
+	position: absolute;
+	/*width: 830px;
+	height: 500px;*/
+	top: 15px;
+	left: 15px;
+	background-color: #000;
+}
 </style>
 <script src="<?= get_template_directory_uri(); ?>/js/waterfall.js"></script>
 <div id="content-all2">    
 	<div id="video-banner">
 		<ul class="slides">
 			<?php foreach(get_posts(array('category_name'=>'video')) as $post){?>
-			<li data-desc="<?php echo $post->post_title; ?>"><a href="<?php echo get_permalink($post->ID); ?>"><?php echo get_the_post_thumbnail($post->ID,'all-video-slide'); ?></a></li>
+			<li data-video="http://qcyh.lc/wp-content/uploads/2013/12/echo-hereweare.mp4" data-desc="<?php echo $post->post_title; ?>"><a href="<?php echo get_permalink($post->ID); ?>"><?php echo get_the_post_thumbnail($post->ID,'all-video-slide'); ?></a></li>
 			<?php } ?>
-
-			<li data-desc="1"><a href="#"><img src="http://www.bing.com/az/hprichbg/rb/EurasianRed_EN-US12831744194_1366x768.jpg" alt=""></a></li>
-			<li data-desc="2"><a href="#"><img src="http://www.bing.com/az/hprichbg/rb/EurasianRed_EN-US12831744194_1366x768.jpg" alt=""></a></li>
-			<li data-desc="3"><a href="#"><img src="http://www.bing.com/az/hprichbg/rb/EurasianRed_EN-US12831744194_1366x768.jpg" alt=""></a></li>
-			<li data-desc="4"><a href="#"><img src="http://www.bing.com/az/hprichbg/rb/EurasianRed_EN-US12831744194_1366x768.jpg" alt=""></a></li>
-			
 		</ul>
 
 		<div class="prev">
@@ -53,14 +74,17 @@ user-select: none;
   <div style=" background:url(images/v-l.png) no-repeat; width: 55px; height:500px; float:left; color: #999;">
   <ul style="list-style:none; width:32px; padding:0; padding-left: 12px; padding-top:134px; margin:0;">
   	<li style="padding-bottom:3px;">分享</li>
-    <li style="padding-bottom:7px; "><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/weibo-icon.jpg" width="32" height="32" style="line-height:0; display:block;"></a></li>
+    <li style="padding-bottom:7px;"><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/weibo-icon.jpg" width="32" height="32" style="line-height:0; display:block;"></a></li>
     <li style="padding-bottom:7px;"><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/renren.jpg" width="32" height="32" style="line-height:0; display:block;"></a></li>
     <li style="padding-bottom:7px;"><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/qq.jpg" width="32" height="32" style="line-height:0; display:block;"></a></li>
     <li style="padding-bottom:7px;"><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/kaixin.jpg" width="32" height="32" style="line-height:0; display:block;"></a></li>
     <li><a href="#"><img src="<?= get_template_directory_uri(); ?>/images/douban.jpg" width="32" height="32"></a></li>
   </ul>
   </div>
-    <div style="width: 848px; height: 500px;float:right;"><img src="<?= get_template_directory_uri(); ?>/images/v-r.png" width="848" height="500"></div>
+  <div class="video-container">
+  	<img src="<?= get_template_directory_uri(); ?>/images/v-r.png" width="848" height="500">
+  	<video width="820" height="470" src="http://qcyh.lc/wp-content/uploads/2013/12/echo-hereweare.mp4"></video>
+  </div>
 </div>
 
 
@@ -177,32 +201,36 @@ next.on("click",function(){
 (function(){
 var shadow = $("#shadow");
 var pop = $("#pop");
-
+var player = new MediaElementPlayer('#pop video');
 shadow.on("click",function(){
 	closeVideo();
 });
 
-$(".slides").on("click","li",function(){
-	openVideo();
+$(".slides").on("click","li",function(e){
+	e.preventDefault();
+	openVideo($(this).attr("data-video"));
 });
 // 视频弹层
-function openVideo(){
+function openVideo(videoSrc,href){
 	pop.show();
+
 	function pos(){
 		pop.css({
 			position:"fixed",
 			top:$(window).height()/2 - 500/2,
-			left:$(window).width()/2 - 900/2
+			left:$(window).width()/2 - 900/2 - 30
 		});
 	}
 	$(window).on("resize",function(){
 		pos();
 	});
+	player.setSrc(videoSrc);
 	shadow.show();
+	pos();
 }
 
 function closeVideo(){
-	alert();
+	player.pause();
 	pop.hide();
 	shadow.hide();
 }
