e4e5751a77e947d9e6630b46391aa69ffc06a156
supersheep <supersheepxjy@gmail.com>
2013-12-26 22:55:30 +0800

    欢迎页滚动100% fix #10

--- wp-content/themes/youth/home.php
+++ wp-content/themes/youth/home.php
@@ -1,8 +1,5 @@
 <?= get_header("simple") ?>
 <style>
-#welcome-bar{
-    position: relative;
-}
 #welcome-bar .welcome_pic{
     position: absolute;
     top: 0;
@@ -11,12 +8,10 @@
 }
 
 #welcome-bar .welcome_pic ul{
-    width:3000px;
     left:0;
     position: absolute;
 }
 #welcome-bigPic{
-    position: relative;
 }
 #enter-btn{
     width:240px;
@@ -27,28 +22,29 @@
 }
 </style>
 <div id="home-wrap">
+<div id="welcome-bar"></div>
+<div class="welcome_pic">
+    <ul>
+        <li>
+            <img src="<?= get_template_directory_uri(); ?>/images/welcome-p1.png" width="148" height="224" />
+        </li>
+        <li>
+            <img src="<?= get_template_directory_uri(); ?>/images/welcome-p2.png" width="148" height="224" />
+        </li>
+        <li>
+            <img src="<?= get_template_directory_uri(); ?>/images/welcome-p3.png" width="148" height="224" />
+        </li>
+        <li>
+            <img src="<?= get_template_directory_uri(); ?>/images/welcome-p4.png" width="148" height="224" />
+        </li>
+    </ul>
+</div>
+
+<div id="welcome-wrap">
 <div id="welcome-bg">
-    <div id="welcome-bar">
         <div id="welcome-bigPic" class="bigPic-welcome">
             <img src="<?= get_template_directory_uri(); ?>/images/welcome-bigPic.png" width="831" height="552" />
-
             <a href="<?php site_url(); ?>/index/" id="enter-btn"></a>
-        </div>
-        <div class="welcome_pic">
-            <ul>
-                <li>
-                    <img src="<?= get_template_directory_uri(); ?>/images/welcome-p1.png" width="148" height="224" />
-                </li>
-                <li>
-                    <img src="<?= get_template_directory_uri(); ?>/images/welcome-p2.png" width="148" height="224" />
-                </li>
-                <li>
-                    <img src="<?= get_template_directory_uri(); ?>/images/welcome-p3.png" width="148" height="224" />
-                </li>
-                <li>
-                    <img src="<?= get_template_directory_uri(); ?>/images/welcome-p4.png" width="148" height="224" />
-                </li>
-            </ul>
         </div>
     </div>
     <div class="index-weibo" style="clear: both; margin-top: 266px; width: 1334px; margin: 0 auto;">
@@ -56,8 +52,9 @@
             <img src="<?= get_template_directory_uri(); ?>/images/index-weibo.png" width="171" height="39" /></a>
     </div>
 </div>
-
+</div>
 <script>
+jQuery.fx.interval = 45;
 (function(){
     var container = $(".welcome_pic");
     var ul = container.find("ul");
@@ -65,13 +62,16 @@
     var count = items.length;
     var one_width = (items.width() + parseInt(items.css("margin-right")));
     var width =  0;
-    var maxwidth = container.width();
+    var maxwidth = $(window).width();
     while(width < maxwidth + one_width * 4){
         for(var i = 0; i < count; i++){
             ul.append(items.eq(i).clone());
             width += one_width;
         }
     }
+    ul.find("li").each(function(i,li){
+        $(li).css("left",i*158);
+    });
 
     function scroll(){
         ul.animate({
