d169672b05f5a1a559099ad80a63b4c23c519b7d
陆秋石 <uicestone@gmail.com>
2013-12-27 22:31:38 +0800

    fix: 欢迎页网页标题多了个横杠

--- wp-content/themes/youth/functions.php
+++ wp-content/themes/youth/functions.php
@@ -14,7 +14,7 @@ add_image_size( 'all-video-waterfall', 190, 0);
 add_image_size( 'product-slide', 1120, 618, true);
 
 add_action('wp_head', function(){
-	echo '<title>'.wp_title('',false).' - '.get_bloginfo('name').'</title>';
+	echo '<title>'.(is_home()?'':wp_title('',false).' - ').get_bloginfo('name').'</title>';
 	echo '<link rel="shortcut icon" href="'.get_stylesheet_directory_uri().'/images/favicon.ico" />';
 });
 
